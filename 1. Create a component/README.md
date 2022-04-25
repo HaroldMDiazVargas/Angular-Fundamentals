9d591917dc4798ddec09d446c88884118b652302
Harold Diaz <harold.diazv1@gmail.com>
2022-04-25 16:53:36 -0500

    Steps to create a new component in angular

---
+++ 1. Create a component/README.md
@@ -0,0 +1,15 @@
+# Command line to create new component!
+
+1. In terminal use ng generate(g) component(c):
+
+```
+ng g c component-name
+```
+
+Inside src/app/component-name/:
+
+- component-name.component.css file -> Storing stylesheet for this component
+- component-name.component.css file -> Storing the template
+- component-name.component.css file -> Writing unit test
+- component-name.component.css file -> Component itself
+  Update app.module.ts and register the component in declarations array.
---
+++ 1. Create a component/courses.component.ts
@@ -0,0 +1,25 @@
+
+//--------------------------------------------------------------------------------------------------------
+//1st Step -Create ts component
+//Decorator Component make a class component
+import { Component } from '@angular/core'; 
+
+@Component({
+    //1 or more properties to tell angular how works(selector,templateUrl,srtyleUrls)
+    selector: 'courses', //css selector
+    template: '<h2>Courses</h2>', //html markup to render for this component
+
+}) //Decorator function
+export class CoursesComponent{  // component-name+Subfix Component
+
+}
+
+//-----------------------------------------------------------------------------------------------------
+//2nd Step-Register(not known error in console)
+//Go to app.module.ts -> declarations arrays(add all components part of this module) -> add here component.
+@NgModule //Decorator function to convert TS plain into module from angular point of view. 
+
+//-----------------------------------------------------------------------------------------------------
+//3rd Step-Render
+//Render the template of courses.component in app.component.html using <courses>
+//The template for app.component.ts is <app-root> includes all other templates and is defined in index.html
