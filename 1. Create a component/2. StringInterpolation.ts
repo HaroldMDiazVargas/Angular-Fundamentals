0c811169eccd210df226013a85a9ca42f67024e2
Harold Diaz <harold.diazv1@gmail.com>
2022-04-25 17:21:27 -0500

    Using string interpolation to render something in template

---
+++ 1. Create a component/2. StringInterpolation.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core'; 
+
+@Component({
+    selector: 'courses', 
+    template: '<h2>{{ getTitle() }}</h2>' // String Interpolation  {{}}
+                                          //Data binding(update DOM) if field change
+
+}) 
+export class CoursesComponent{
+    title = "List of Courses";
+    courses 
+
+    getTitle() {
+        return this.title;
+    }
+
+}
+`` //BackTick 
