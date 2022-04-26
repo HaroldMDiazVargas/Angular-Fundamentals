58331e820ca11baa63698037354232e6e06d7085
Harold Diaz <harold.diazv1@gmail.com>
2022-04-25 20:12:45 -0500

    Inject service to AuthorsComponent constructor

--- 1. Create a component/demo/src/app/authors/authors.component.ts
+++ 1. Create a component/demo/src/app/authors/authors.component.ts
@@ -9,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthorsComponent implements OnInit {
   authors;
-  constructor(service:EmailService) {
+  constructor(service:EmailService ) {
           this.authors = service.getAuthor();
    }
 
