c4eeac097a981d2ac9f2f464160b30339344de7f
Harold Diaz <harold.diazv1@gmail.com>
2022-04-25 20:10:28 -0500

    Create email.service with ng cli

---
+++ 1. Create a component/demo/src/app/email.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmailService {
+  authors = ['author1','author2','author3'];
+  // constructor() { }
+
+  getAuthor(){
+    return this.authors;
+  }
+}
