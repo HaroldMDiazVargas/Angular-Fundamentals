fe22dd64fd94fe46bf2e7accf74f511f27dd5be4
Harold Diaz <harold.diazv1@gmail.com>
2022-04-25 20:09:53 -0500

    Create authors.component with ng cli

---
+++ 1. Create a component/demo/src/app/authors/authors.component.ts
@@ -0,0 +1,19 @@
+import { EmailService } from './../email.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'authors',
+  templateUrl: './authors.component.html',
+  // template: ,
+  
+})
+export class AuthorsComponent implements OnInit {
+  authors;
+  constructor(service:EmailService) {
+          this.authors = service.getAuthor();
+   }
+
+  ngOnInit(): void {
+  }
+
+}
