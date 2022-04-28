4acd00512cf0951eb92ab4da976a0cbf5c2b9efb
Harold Diaz <harold.diazv1@gmail.com>
2022-04-27 20:14:52 -0500

    Logic of title-case pipe to capitalize title

--- 1. Create a component/demo/src/app/title-case.pipe.ts
+++ 1. Create a component/demo/src/app/title-case.pipe.ts
@@ -6,9 +6,29 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 export class TitleCase implements PipeTransform {
     transform(value:string, args?:any){
-        if(!value)
-            return null;
-        return value;
+        if (!value) return null;
+        
+    
+        let words = value.split(' ');
+        for (var i = 0; i < words.length; i++) {
+          let word = words[i];
+          if (i > 0 && this.isPreposition(word))
+            words[i] = word.toLowerCase();
+          else 
+            words[i] = this.toTitleCase(word);
+        }
+    
+        return words.join(' ');
     }
+    
+      private toTitleCase(word: string): string {
+        return word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase();
+      }
+      
+      private isPreposition(word: string): boolean {
+        let prepositions = ["of", "on", "and", "or", "in", "a", "the"];
+    
+        return prepositions.includes(word.toLowerCase());
+      }
 
 }
