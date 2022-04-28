d34c8c9751f42f2e80191cd7333818100425a8a5
Harold Diaz <harold.diazv1@gmail.com>
2022-04-28 18:31:22 -0500

    Like component with two input properties

--- 1. Create a component/demo/src/app/app.component.ts
+++ 1. Create a component/demo/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo';
+
+  tweet = {
+    body: 'Here is the content tweet...',
+    isLiked: false,
+    likesCount: 10
+  }
 }
--- 1. Create a component/demo/src/app/like/like.component.ts
+++ 1. Create a component/demo/src/app/like/like.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-like',
+  selector: 'like',
   templateUrl: './like.component.html',
   styleUrls: ['./like.component.css']
 })
 export class LikeComponent implements OnInit {
 
+  @Input("isSelected") isSelected = false;
+  @Input("likesCount") likesCount = 0;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  clicked(){
+    this.likesCount += this.isSelected ? -1:1;
+    this.isSelected = !this.isSelected;
+    // this.likesCount === 0 ? this.likesCount++:this.likesCount--;
   }
 
 }
