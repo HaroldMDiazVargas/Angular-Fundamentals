e77b55590cbc721eeaefe6f3a4e7f22f812e1020
Harold Diaz <harold.diazv1@gmail.com>
2022-04-29 18:20:06 -0500

    Using ngModelGroup directive to add complex object to value object

--- 5. Template-drivenForms/9. ngModelGroup.ts
+++ 5. Template-drivenForms/9. ngModelGroup.ts
@@ -1,24 +1,25 @@
 // Complex forms is better have multiples groups in the form
 
 
-// Before we have value Object => comment: "Comment", firstName:"banana"
+// Before we had value Object => {comment: "Comment", firstName:"banana"}
 // Is desirable to have value Object => comment: "assaf", contact(complex object)
-// contact object has => firstName: "harold" => Potentially later we can have lasName, email, etc.
+// contact object has => firstName: "harold" => Potentially later we can have lasName, email, etc as keys in this complex object.
 // other complex object we may want to have is address Object => shipping, billing, etc.
 
-// We have ngModelGroup Directive =>  Represent the subproperty in our value Object
+// We have ngModelGroup Directive =>  Represent the subproperty in our value Object => This directive creates an instance of FormGroup class for that group
 // In our app the API we have in server may expect a complete nested object structure like this => Use ngModelGroup
 // ngModelGroup = "valueOfngModelGroup"(subproperty in our value object)
 
 // Also using #variableName to reference entire ngModelGroup to validated it => e.x display validation erors on top of billing group
 
-
-
-
 // Value object = { comment: "Comment"
 //                 contact: { firstName: "banana"  } }
 
 
+
+// Difference between ngForm and ngModelGroup:
+// ngForm exposes an output property (ngSubmit) => to handle submit event of forms
+// ngModelGroup doesn't have the output propertu => Ilogical to submit part of a form
 
 <form #f="ngForm" (ngSubmit)="submit(f)">
     <div ngModelGroup="contact" #contact="ngModelGroup">                    //Add complex contact object to value
