5c5c492e94cfcf16ab348c5284a3e857301f7de7
Harold Diaz <harold.diazv1@gmail.com>
2022-05-04 17:43:13 -0500

    Using classes and catch operator to throwing specific errors

--- 7. ConsumingHTTPServices/12. ThrowingApp-specificErrors.ts
+++ 7. ConsumingHTTPServices/12. ThrowingApp-specificErrors.ts
@@ -26,7 +26,7 @@
 
 // Create a new class in app/common/app-error.ts to represent application specific errors
 // In the class => Export class AppError => Represent an app error
-// So, as arguent to Observable.throw(AppErroObject)
+// So, as arguument to Observable.throw(AppErroObject)
 // Is good practica to include original error inside AppError instance => Somewhere we get that error and log on server
 // So, pass original error => Finally rreturn the results
 
@@ -38,15 +38,23 @@
 // In common/ folder => add new faile not-found-error.ts
 // In the class => Export class NotFoundError  => Derivate from AppError class => kind of app error but more specific
 // Now => check the status of server => 404 => return Observable.throw(NotFounError Instance)
-// In this case not put the original error as well => We're not going to log this to the server => This is legitime error not an unexpected error
+// In this case not put the original error => We're not going to log this to the server => This is legitime error(not an unexpected error) 
 
 // Las stept => In our component
 // Instead of working with the (error:Response) object => work with AppError or one of its derivities
-// Because in post.service => throwind either AppError or NotFoundError
-// So, in our component => Response should be AppError => And check if error is an instance of NotFoundError
+// Because in post.service => throwing either AppError or NotFoundError
+// (Observable.throw(new NotFoundError() or new AppError(error)) => Essentially we're catching a Response Object and throwing and AppErrorObject 
+// So, in our component type of the error => Response should be AppError => And check if error is an instance of NotFoundError
 // In jscript => we have an operator called 'instanceof'
 
+// ---------------------For create method---------------------------------------
 // Now the same for thhe createMethod => Instead of working with the Response object and chek status is 400 => Better work with App Specific Errors
+// Create in/common/ bad-input.ts => Input to differentiate from http protocol(which use request word)
+// Create class BadInput extends AppError 
+// The argment for newBadInput(error.json) => Include error object came from the server => error object includes data about the invalid fields
+// Otherwise pass generic AppError object with argument => original error object => For logging later
+
+// We have improved the separation of concern in our code => In post.component.ts=> Not longer work with the Response Object or HTTP 
 
 
 // post.service.ts
@@ -64,7 +72,12 @@ export class PostService{
     }
 
     createPost(post){
-        return this.http.post(this.url, JSON.stringify(post));
+        return this.http.post(this.url, JSON.stringify(post))
+        .catch((error:Response) => {        // Error function => takes and error of type Response (goes to) code block
+            if(error.status === 400)
+                return Observable.throw(new BadInput(error.json()));
+            return Observable.throw(new AppError(error.json()))
+        });
     }
 
     updatePost(post){
@@ -75,7 +88,7 @@ export class PostService{
         return this.http.delete(this.url +'/'+ id)
         .catch((error:Response) =>{
             if(error.status === 404)
-                return Observable.throw(new NotFoundError());
+                return Observable.throw(new NotFoundError());Observable.throw(new AppError(error))
             return Observable.throw(new AppError(error));
 
         });
@@ -129,9 +142,9 @@ export class PostsComponent implement OnInit {
                 this.posts.splice(0,0,post);
                 console.log(response.json());
             },
-            (error:Response) =>{
-                if(error.status === 400)
-                    this.form.setErrors(error.json());
+            (error: AppError) =>{
+                if(error instanceof BadInput)
+                    this.form.setErrors(error.originalError); // Comment this line if not have a form
                 else
                     alert('An unexpected error occured');   
                     console.log(error);                            
@@ -159,7 +172,7 @@ export class PostsComponent implement OnInit {
                 let index = this.posts.indexOf(post);
                 this.posts.splice(index,1);  
             },
-             (error:Response) =>{
+             (error:AppError) =>{
                 // if(error.status === 404)
                 if(error instanceof NotFoundError)
                     alert('This post has already been deleted.');
@@ -183,5 +196,12 @@ export class AppError{
 // In not-found-error.ts class => Derivate from app-erro class
 
 export  class NotFoundError extends AppError{
+
+}
+
+
+// In bad-request-error.ts class => Derivate from app-erro class
+
+export  class BadInput extends AppError{
 
 }
