28fe89e4c8dfaeb9436b639a604cf7935f670567
Harold Diaz <harold.diazv1@gmail.com>
2022-05-06 12:20:14 -0500

    Steps to implement navigation

---
+++ 8. RoutingAndNavigation/1. RoutingInANutshell.ts
@@ -0,0 +1,13 @@
+// We have seen a few of the Built-in modules in Angular:
+// Forms Module
+// ReactiveForms Module
+// Http Module
+// Router Module => Time to explore it
+
+// In Router Module we have a bunch of Directive and Services => We use for  implementing navigation in our app
+// There're three steps to implement navigation:
+// 1. Configure the routes in our app => Each route determinates what component should be visible when user navigate to a certain url
+//    A route is the mapping of the path to a component
+// 2. Add a router outlet => Where we display the corresponding component when a given route become active 
+// 3. Add links
+
