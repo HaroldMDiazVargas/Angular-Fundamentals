442baafbc05ae3303df25724c831ea0306f3131b
Harold Diaz <harold.diazv1@gmail.com>
2022-05-08 22:09:40 -0500

    Using TSLint extension in VSCode

---
+++ 10. Deployment/8. LintingInVSCode.ts
@@ -0,0 +1,12 @@
+//Lint our code directly in VSCode
+
+// Install TSLint extension 
+// So we can see e.x trailing whitespace indicated by red underline 
+// other e.x if we change string to double quotes => we can see a left command => to activate it => Ctrl+. => suggest to fix double quote to single or suggest disable rule
+// A faster way to fix errors in a file => using the command pallete => shift+ctrl+p => search tslint
+// we can see 3 commands:
+// TSLint: Fix all auto-fixable problems => with this fix all issues in one go
+// TSLint: Create 'tslint.json' file
+// TSLint: Show output
+
+// But even with this extension is a good practice to run in terminal lint => before commit to our repository
