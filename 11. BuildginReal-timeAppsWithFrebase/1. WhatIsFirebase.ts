5b6369ed307f7432704e148653f43824fa81900e
Harold Diaz <harold.diazv1@gmail.com>
2022-05-09 19:20:40 -0500

    Explanation about Firebase cloud platform

---
+++ 11. BuildginReal-timeAppsWithFrebase/1. WhatIsFirebase.ts
@@ -0,0 +1,23 @@
+// The architecture of an app => consist of two parts 
+// 1. Front-end => Sits on client => We use angularto build it
+// 2. Back-end => Sits on the server => We use different set of languages and frameworks to build it 
+// The frontend talks to backend to get or save data by of a bunch of HTTP Services 
+// Behind these HTTP Services we have 1 or more databases => such as => SQL Server, Mongo, Raven, so on.
+
+// Front-end -----------> Back-end ------------> Database
+//           <---------- Back-end  <------------
+
+// This is the architecture of most app out there
+
+// As a front-end developer => dont worry about the backend
+// Building backend is time consuming => that's where we Firebase shines
+// With firebase => we can build our backend for our angular apps => in less than 1 minute 
+//               => super fast, scalable and real-time on the cloud => and a library to talks to this in angular apps
+//               => On top of that, we also get authentication
+//               => cloud messagin => for sending notification
+//               => storage => for storing our files
+//               => analytics
+//               => Provide libraries for iOs, Android, JS and C++
+//               => We can get most of these features for free => If our app grows => we can either pay 20 usd at month or subscription modal
+
+
