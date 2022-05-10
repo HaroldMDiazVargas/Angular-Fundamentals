7a87d112c47f2e57b37b8504c7c61f1a4873f25e
Harold Diaz <harold.diazv1@gmail.com>
2022-05-09 19:04:31 -0500

    Adding engines key value to set version of npm and node used by heroku

---
+++ 10. Deployment/13. Engines.ts
@@ -0,0 +1,11 @@
+// There's one more stept for deploy our app to Heroku
+// this stept is not really required => but is somethind recommendable to prevent any potential problems
+
+// Go to package.json
+// scroll to button => add new key called "engines" => set the version of node and npm that we have on our local machine => and heroku is going to use the exact same versions
+// },
+// "engines":{"node":"6.10.3", "npm":"5.3.0"} 
+//}
+//}
+
+//Deploy the app again
