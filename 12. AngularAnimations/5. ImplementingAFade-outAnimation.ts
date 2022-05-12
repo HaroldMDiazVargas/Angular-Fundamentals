1478a4af70b58ad8c5921c10cbafbd2e64380e47
Harold Diaz <harold.diazv1@gmail.com>
2022-05-11 19:35:10 -0500

    Implementing a transition from *state to void state

---
+++ 12. AngularAnimations/5. ImplementingAFade-outAnimation.ts
@@ -0,0 +1,141 @@
+// If we click over a list item => it disappear without animation
+
+// In the animation metada property:
+// We defined a previously a trigger called fade => We have a transition from the void state to the default state
+// Now, lets define another transition from the default state => to the void state
+//  Just define a second transition('* => void', []) 
+// For the steps => we want to change the opacity of this element => from 1 => to 0 => over a period of time
+//               => animate(2000, style({opacity:0 }))  => this is all we have to do
+// Remember separe with comma ',' the different transitions 
+
+
+
+// ----------------------  todos.component-------------------------------------------------
+
+
+//markup
+<h1>Todos</h1>
+
+<input #itemInput
+  class="form-control"
+  (keyup.enter)="addItem(itemInput)">
+
+<div *ngIf="items" class="list-group" >
+  <button type="button"
+       @fade                                            // Apply the trigger
+      *ngFor="let item of items"
+      (click)="removeItem(item)"
+      class="list-group-item"
+      >{{ item }}</button>
+</div> 
+
+
+
+//typescript
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css'],
+  animations: [
+      trigger('fade',[ 
+
+          transition('void => *', [
+            style({opacity:0}),
+            animate(2000)  
+            
+      ]),
+        transition('* => void', [
+            animate(2000, { opacity:0 })
+        ]) 
+    ])
+  ]
+})
+export class TodosComponent {
+  items: any[] = [
+    'Wash the dishes', 
+    'Call the accountant', 
+    'Apply for a car insurance'];
+
+  addItem(input: HTMLInputElement) {
+    this.items.splice(0, 0, input.value);
+    input.value = ''; 
+  }
+
+  removeItem(item) {
+    let index = this.items.indexOf(item);
+    this.items.splice(index, 1);
+  }
+}
+
+
+//---------------------------------zippy component--------------------------------------
+
+// markup
+
+<div class="zippy">
+  <div 
+    class="zippy-heading"
+    [class.expanded]="isExpanded"
+    (click)="toggle()">
+    {{ title }}
+    <span class="glyphicon"
+      [ngClass]="{
+        'glyphicon-chevron-up': isExpanded,
+        'glyphicon-chevron-down': !isExpanded
+      }"
+    ></span>
+  </div>
+  <div class="zippy-body" [hidden]="!isExpanded">
+    <ng-content></ng-content>
+  </div>
+</div>
+
+// stylesheet
+
+.zippy { 
+    border: 1px solid #ccc;
+    border-radius: 2px;
+}
+
+.zippy-heading { 
+    font-weight: bold;
+    padding: 20px;
+    cursor: pointer;
+}
+
+.zippy-body { 
+    padding: 20px;
+    overflow: hidden;
+}
+
+.expanded { 
+    background: #f0f0f0;
+}
+
+.glyphicon { 
+    float: right;
+}
+
+
+//typescript
+
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'zippy',
+  templateUrl: './zippy.component.html',
+  styleUrls: ['./zippy.component.css'],
+})
+export class ZippyComponent  {
+  @Input('title') title: string;
+  isExpanded: boolean;
+
+  toggle() { 
+    this.isExpanded = !this.isExpanded;
+  }
+}
+
+
