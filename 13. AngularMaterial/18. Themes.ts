f02763a9a69922e6d992ceee4a3577bfe8a457f4
Harold Diaz <harold.diazv1@gmail.com>
2022-05-14 19:07:03 -0500

    Color tool to define color palette of a custom theme

---
+++ 13. AngularMaterial/18. Themes.ts
@@ -0,0 +1,33 @@
+// Themes
+
+// In Angular Material we have a concept called => Theme
+// Theme is basically a collection of various color palettes
+// We have the following color palettes:
+//  - Primary => Refers to a color that appears most frequently in our app
+//  - Secondary => Use to give some accent to certain key elements in the UI => e.x often used for buttons, progress bars, sliders, highited things
+//  - Warn => Give warnings to the user => e.x error in an input field
+
+// We can have for example => 3 shades of the primary color => mid-tone and a dark shade, light shade 
+// This color palette helps us to keep consistly look in our app
+// So if in a future we want to modify the primary color => we just need to modify in 1 place
+//                                                       => Then all our components will automatically adopte the new color palette
+
+// We can import a theme or create a custom theme that gives character to our app 
+// To design the color palette => we need to use the color tool on => material.io/color website => color tool
+
+
+// Color tool 
+// In the left side => we can navigate and see different example of User Interfaces
+//                  => to give us an idea how our theme looks like
+// Look on the top of color palette => every color has different versions or shapes
+//                                  => 50(lightest) and 900(darkest)
+// In right-below side => we have primary and secondary => to select the secondary we need to select a primary color first
+// Example:
+// primary color => blue =>  scroll down and find the blue => select one => 600 blue
+// secondary color => lets use yellow 
+//                 
+// We need to remember the number(version) of the color  => because we need to reference it exactly
+//                                              => when we're going to make a custom theme
+//                                              => All these colors are predefined in Material Design guideline => they all have name and version
+//                              
+
